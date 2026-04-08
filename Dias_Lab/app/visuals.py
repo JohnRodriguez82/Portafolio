b6054f1001cbf8fd8d46ca6216c90abceeb4b6de
JohnRodriguez82 <42385693+JohnRodriguez82@users.noreply.github.com>
2026-04-08 16:54:19 -0500

    Update visuals.py

--- Dias_Lab/app/visuals.py
+++ Dias_Lab/app/visuals.py
@@ -1,1 +1,12 @@
+import altair as alt
 
+def grafica_barras(df):
+    resumen = df.groupby("SECCION")["dias_num"].mean().reset_index()
+
+    chart = alt.Chart(resumen).mark_bar().encode(
+        x="SECCION",
+        y="dias_num",
+        tooltip=["SECCION","dias_num"]
+    ).properties(title="Promedio de días por sección")
+
+    return chart
