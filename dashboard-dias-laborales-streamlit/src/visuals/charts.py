3121d9a764b90359003631935e2434f290d8df1f
JohnRodriguez82 <42385693+JohnRodriguez82@users.noreply.github.com>
2026-04-10 23:19:37 -0500

    Update charts.py

--- dashboard-dias-laborales-streamlit/src/visuals/charts.py
+++ dashboard-dias-laborales-streamlit/src/visuals/charts.py
@@ -69,7 +69,7 @@ def render_charts(df):
         fontSize=13,
         align="center",
         baseline="top",
-        dy=-18,
+        dy=-22,
         stroke="white",
         strokeOpacity=0.7,
         strokeWidth=1,
